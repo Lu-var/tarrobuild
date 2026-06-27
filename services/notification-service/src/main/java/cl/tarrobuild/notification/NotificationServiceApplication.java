04b5d9e635cf1d99c7d6d3e9e7acc090bede68c5
camila <ca.corocedo@duocuc.cl>
2026-06-26 23:06:43 -0400

    feat: incluir UserRestClient y NotificationService con resolucion de correo

--- services/notification-service/src/main/java/cl/tarrobuild/notification/NotificationServiceApplication.java
+++ services/notification-service/src/main/java/cl/tarrobuild/notification/NotificationServiceApplication.java
@@ -2,12 +2,12 @@ package cl.tarrobuild.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class NotificationServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
-
 }
