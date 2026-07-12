16b088c21d018adc522cd522833fbde69f8f60f5
lu-var <luc.vargasp@duocuc.cl>
2026-07-11 21:15:23 -0400

    feat: discovery-server Eureka + registro de servicios en Eureka

--- services/notification-service/src/main/java/cl/tarrobuild/notification/NotificationServiceApplication.java
+++ services/notification-service/src/main/java/cl/tarrobuild/notification/NotificationServiceApplication.java
@@ -2,9 +2,11 @@ package cl.tarrobuild.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableAsync
 public class NotificationServiceApplication {
     public static void main(String[] args) {
