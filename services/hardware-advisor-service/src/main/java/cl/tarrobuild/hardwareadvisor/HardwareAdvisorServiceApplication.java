9219879897f9e25b8a6977e94155c10f652b41a1
lu-var <luc.vargasp@duocuc.cl>
2026-07-11 21:15:23 -0400

    feat: discovery-server Eureka + registro de servicios en Eureka

--- services/hardware-advisor-service/src/main/java/cl/tarrobuild/hardwareadvisor/HardwareAdvisorServiceApplication.java
+++ services/hardware-advisor-service/src/main/java/cl/tarrobuild/hardwareadvisor/HardwareAdvisorServiceApplication.java
@@ -3,8 +3,10 @@ package cl.tarrobuild.hardwareadvisor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 public class HardwareAdvisorServiceApplication {
 
