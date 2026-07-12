83e9f61573fc853bdea2db97cc3c9a4c446e9b69
Camila Corocedo Lorca <ca.corocedo@duocuc.cl>
2026-07-11 21:32:54 -0400

    feat: FeignClients con lb:// service discovery

--- services/hardware-advisor-service/src/main/java/cl/tarrobuild/hardwareadvisor/client/BuildFeignClient.java
+++ services/hardware-advisor-service/src/main/java/cl/tarrobuild/hardwareadvisor/client/BuildFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "build-service", url = "${build-service.url}", fallbackFactory = BuildFeignClientFallbackFactory.class)
+@FeignClient(name = "build-service", fallbackFactory = BuildFeignClientFallbackFactory.class)
 public interface BuildFeignClient {
     @GetMapping("/api/builds/{id}")
     BuildClientResponse getBuildById(@PathVariable Long id);
--- services/hardware-advisor-service/src/main/java/cl/tarrobuild/hardwareadvisor/client/CompatibilityFeignClient.java
+++ services/hardware-advisor-service/src/main/java/cl/tarrobuild/hardwareadvisor/client/CompatibilityFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "compatibility-service", url = "${compatibility-service.url}", fallbackFactory = CompatibilityFeignClientFallbackFactory.class)
+@FeignClient(name = "compatibility-service", fallbackFactory = CompatibilityFeignClientFallbackFactory.class)
 public interface CompatibilityFeignClient {
     @PostMapping("/api/compatibility/check")
     CompatibilityCheckResponse checkCompatibility(@RequestBody CompatibilityCheckRequest request);
